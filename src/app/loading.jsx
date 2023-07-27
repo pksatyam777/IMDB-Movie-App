442152306dc5171583c22bec85e6e0ac4c623aa0
pksatyam777 <pksatyam777@gmail.com>
2023-07-27 20:28:57 +0530

    added loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className="items-center"><img className="h-96 " src="loading.svg" alt="loader..." /></div>
+  )
+}
