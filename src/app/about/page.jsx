106cb40920bc1c2db2ae19da16f83655368bb51b
pksatyam777 <pksatyam777@gmail.com>
2023-07-28 13:17:57 +0530

    FIX STRING

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,35 +9,47 @@ export default function About() {
         Welcome to Cinemate, the ultimate movie searching app that brings the
         magic of cinema right to your fingertips. Step into a world of cinematic
         wonders as Cinemate opens its virtual doors to cinephiles and casual
-        moviegoers alike.</p><p> The app boasts an intuitive user interface that makes
-        searching for movies an absolute breeze. With its sleek design and
-        eye-catching graphics, you'll feel like you're stepping into your very
-        own digital movie theater. Let Cinemate be your trusty guide through the
-        vast universe of movies. Want to explore the latest blockbusters or dive
-        into timeless classics? Cinemate has got you covered. Simply type in the
-        name of your favorite actor, director, or genre, and let the app work
-        its magic. Within seconds, a treasure trove of movie recommendations
-        tailored to your preferences will be at your disposal.</p><p> But Cinemate is
-        not just your average run-of-the-mill movie searching app. It comes
-        packed with a plethora of exciting features that will leave you hooked.
-        Say goodbye to endless scrolling and hello to personalized watchlists.
-        Create your profile and curate a collection of must-watch movies that
-        you can revisit whenever the mood strikes. Feeling adventurous? Let
-        Cinemate surprise you with its "Movie Roulette" feature. Simply shake
-        your device, and the app will pick a random movie for you to enjoy. It's
-        like having your very own digital movie genie! Cinemate also embraces
-        the social side of cinema. Connect with fellow movie enthusiasts, share
-        your thoughts on films, and engage in lively discussions.</p><p> You can even
-        plan movie nights with friends using the integrated group watch feature.
-        Worried about running out of movies to watch? Fret not! Cinemate
-        regularly updates its library, ensuring you never miss the latest
-        releases or hidden gems. Additionally, the app provides insightful
-        reviews, ratings, and trailers to help you make informed decisions about
-        your movie selections. So, whether you're a die-hard cinephile or just
-        someone looking for a cozy movie night, Cinemate has something for
-        everyone. Embark on a cinematic journey like never before with Cinemate
-        - your one-stop destination for all things film. Download the app now
-        and let the magic of movies unfold before your eyes. Happy watching!
+        moviegoers alike.
+      </p>
+      <p>
+        {" "}
+        The app boasts an intuitive user interface that makes searching for
+        movies an absolute breeze. With its sleek design and eye-catching
+        graphics, you'll feel like you're stepping into your very own digital
+        movie theater. Let Cinemate be your trusty guide through the vast
+        universe of movies. Want to explore the latest blockbusters or dive into
+        timeless classics? Cinemate has got you covered. Simply type in the name
+        of your favorite actor, director, or genre, and let the app work its
+        magic. Within seconds, a treasure trove of movie recommendations
+        tailored to your preferences will be at your disposal.
+      </p>
+      <p>
+        {" "}
+        But Cinemate is not just your average run-of-the-mill movie searching
+        app. It comes packed with a plethora of exciting features that will
+        leave you hooked. Say goodbye to endless scrolling and hello to
+        personalized watchlists. Create your profile and curate a collection of
+        must-watch movies that you can revisit whenever the mood strikes.
+        Feeling adventurous? Let Cinemate surprise you with its "Movie Roulette"
+        feature. Simply shake your device, and the app will pick a random movie
+        for you to enjoy. It's like having your very own digital movie genie!
+        Cinemate also embraces the social side of cinema. Connect with fellow
+        movie enthusiasts, share your thoughts on films, and engage in lively
+        discussions.
+      </p>
+      <p>
+        {" "}
+        You can even plan movie nights with friends using the integrated group
+        watch feature. Worried about running out of movies to watch? Fret not!
+        Cinemate regularly updates its library, ensuring you never miss the
+        latest releases or hidden gems. Additionally, the app provides
+        insightful reviews, ratings, and trailers to help you make informed
+        decisions about your movie selections. So, whether you're a die-hard
+        cinephile or just someone looking for a cozy movie night, Cinemate has
+        something for everyone. Embark on a cinematic journey like never before
+        with Cinemate - your one-stop destination for all things film. Download
+        the app now and let the magic of movies unfold before your eyes. Happy
+        watching!
       </p>
     </div>
   );
